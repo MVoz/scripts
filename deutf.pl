48062fbdd775d96a430ce14c46a08e073986c12e
liosha <xliosha@gmail.com>
2014-06-13 17:14:45 +0400

    deutf

---
+++ deutf.pl
@@ -0,0 +1,21 @@
+use 5.010;
+use strict;
+use warnings;
+
+use utf8;
+use Encode;
+use Encode::Locale;
+use File::Copy;
+
+for my $file_loc ( glob q{*} ) {
+    my $file_utf = decode locale_fs => $file_loc;
+    my $file_deutf = decode utf8 => $file_loc;
+
+    next if $file_deutf eq $file_utf;
+    
+    my $file_new = encode locale_fs => $file_deutf;
+    next if $file_new =~ m{\?}xms;
+
+    say encode console_out => "$file_utf  -->  $file_deutf";
+    move $file_loc => $file_new;
+}

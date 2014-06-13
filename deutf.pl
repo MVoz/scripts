555356c7664df2a86e4967f200b42063d9ff8280
liosha <xliosha@gmail.com>
2014-06-13 17:20:17 +0400

    pod

--- deutf.pl
+++ deutf.pl
@@ -1,3 +1,15 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+    deutf.pl
+
+=head1 DESCRIPTION
+
+    Rename files with utf8 names in non-utf8 filesystems (useful for windows).
+
+=cut
+
 use 5.010;
 use strict;
 use warnings;
@@ -12,7 +24,7 @@ for my $file_loc ( glob q{*} ) {
     my $file_deutf = decode utf8 => $file_loc;
 
     next if $file_deutf eq $file_utf;
-    
+
     my $file_new = encode locale_fs => $file_deutf;
     next if $file_new =~ m{\?}xms;
 

2d242faefdf5b5601af54297adc3a859de462e24
liosha <xliosha@gmail.com>
2015-03-12 11:28:44 +0300

    tax calculation (without exotic cases)

--- deposit_calc.pl
+++ deposit_calc.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/env perl
 
 use uni::perl;
+use Encode;
+use Encode::Locale;
 
 use Date::Calc qw(:all);
 use Math::Round qw/nearest/;
 use Getopt::Long;
 
 use Date::Holidays::RU qw/is_business_day/;
+use List::Util qw/first/;
 use List::MoreUtils qw/none/;
 
 use Log::Any '$log';
@@ -22,8 +25,8 @@ our %PERC_SUB = (
 ); 
 $PERC_SUB{monthly} = $PERC_SUB{last_business_day_of_month};
 
-
 my %opt;
+Encode::Locale::decode_argv();
 GetOptions(
     'trace' => sub { Log::Any::Adapter->set('Stderr') },
     'start=s' => \$opt{start},
@@ -35,6 +38,7 @@ GetOptions(
     'mode=s' => \$opt{perc_mode},
     'replenishment|repl=s%' => ($opt{replenishments} //= {}),
     'quantum=f' => \$QUANTUM,
+    'currency=s' => \$opt{currency},
 );
 
 say sprintf 'Finally: %.2f', dep_calc(%opt);
@@ -63,6 +67,7 @@ sub dep_calc {
     
     my $sum = $opt{sum} || 0;
     my $sum_perc = 0;
+    my $sum_tax = 0;
 
     my $perc_mode = $opt{perc_mode} || 'at_the_end';
     my $perc_sub = $PERC_SUB{$perc_mode}  or croak "wrong perc_mode: $perc_mode";
@@ -78,10 +83,14 @@ sub dep_calc {
 
         $rate = $rate_change->{$date_key}  if exists $rate_change->{$date_key};
 
-        my $d_perc = nearest $QUANTUM, $sum * ($rate/100 / (365+!!leap_year($date->[0])));
+        my $d_perc = $sum * ($rate/100 / (365+!!leap_year($date->[0])));
         $sum_perc += $d_perc;
+        $sum_tax += $d_perc * _get_RU_tax_rate($date, $rate, $opt{currency});
 
-        $log->trace(sprintf "%s: %.2f   %%: +%.2f -> %.2f", _date_key($date), $sum, $d_perc, $sum_perc)  if $log->is_trace;
+        $log->trace(
+            sprintf "%s: %.2f   %%: +%.2f -> %.2f   tax: %.2f",
+            _date_key($date), $sum, $d_perc, $sum_perc, $sum_tax
+        )  if $log->is_trace;
 
         if ( $repl->{$date_key} ) {
             $sum += $repl->{$date_key};
@@ -89,8 +98,9 @@ sub dep_calc {
         }
 
         if ($perc_sub->($date) || $step_day==$num_days) {
-            $sum += $sum_perc;
+            $sum += $sum_perc - $sum_tax;
             $sum_perc = 0;
+            $sum_tax = 0;
             $log->trace(sprintf "            %.2f   %%: %.2f", $sum, $sum_perc)  if $log->is_trace;
         }
 
@@ -114,6 +124,7 @@ sub _date_from_key {
     my @date;
     if (@date = $key =~ /(\d{4})-(\d{1,2})-(\d{1,2})/xms) {}
     elsif (@date = reverse ($key =~ /(\d{1,2}).(\d{1,2}).(\d{4})/xms)) {}
+    elsif (@date = _parse_RU_date($key)) {}
     else { croak "unreadable date: $key" }
 
     croak "invalid date: $key"  if !check_date(@date);
@@ -148,4 +159,145 @@ sub is_last_business_day_of_month {
     return 1  if none {is_business_day($y, $m, $_)} ($d+1 .. $maxd);
 
     return 0;
+}
+
+
+sub _parse_RU_date {
+    my ($date) = @_;
+    
+    state $months_gen = [ qw/ января февраля марта апреля мая июня июля августа сентября октября ноября декабря / ];
+    state $month_by_name = +{ map {($months_gen->[$_] => $_+1)} (0 .. $#$months_gen) };
+    state $month_re = join q{|}, @$months_gen;
+    state $date_re = qr/ \b (\d{1,2}) \s+ ($month_re) \s+ (\d{4}) (?= \b | г) /xms;
+
+    if (my ($d, $mname, $y) = $date =~ $date_re) {
+        return ($y, $month_by_name->{$mname}, $d);
+    }
+
+    return;
+}
+
+
+sub _get_RU_bank_rate {
+    my ($date) = @_;
+
+    state $bank_rate_changes = [
+        # http://www.operbank.ru/stavka-refinansirovaniya-4.html
+        sort {$b->[0] cmp $a->[0]}
+        map {[ _date_key([_parse_RU_date($_->[0])]) => $_->[1] ]}
+        (
+            ['с 14 сентября 2012 г.' => 8.25],
+            ['с 26 декабря 2011 г.' => 8],
+            ['с 3 мая 2011 г.' => 8.25],
+            ['с 28 февраля 2011 г.' => 8],
+            ['с 1 июня 2010 г.' => 7.75],
+            ['с 30 апреля 2010 г.' => 8],
+            ['с 29 марта 2010 г.' => 8.25],
+            ['с 24 февраля 2010 г.' => 8.5],
+            ['с 28 декабря 2009 г.' => 8.75],
+            ['с 25 ноября 2009 г.' => 9],
+            ['с 30 октября 2009 г.' => 9.5],
+            ['с 30 сентября 2009 г.' => 10],
+            ['с 15 сентября 2009 г.' => 10.5],
+            ['с 10 августа 2009 г.' => 10.75],
+            ['с 13 июля 2009 г.' => 11],
+            ['с 5 июня 2009 г.' => 11.5],
+            ['с 14 мая 2009 г.' => 12],
+            ['с 24 апреля 2009 г.' => 12.5],
+            ['с 1 декабря 2008 г.' => 13],
+            ['с 12 ноября 2008 г.' => 12],
+            ['с 14 июля 2008 г.' => 11],
+            ['с 10 июня 2008 г.' => 10.75],
+            ['с 29 апреля 2008 г.' => 10.5],
+            ['с 4 февраля 2008 г.' => 10.25],
+            ['с 19 июня 2007 г.' => 10],
+            ['с 29 января 2007 г.' => 10.5],
+            ['с 23 октября 2006 г.' => 11],
+            ['с 26 июня 2006 г.' => 11.5],
+            ['с 26 декабря 2005 г.' => 12],
+            ['с 15 июня 2004 г.' => 13],
+            ['с 15 января 2004 г.' => 14],
+            ['с 21 июня 2003 г.' => 16],
+            ['с 17 февраля 2003 г.' => 18],
+            ['с 7 августа 2002 г.' => 21],
+            ['с 9 апреля 2002 г.' => 23],
+            ['с 4 ноября 2000 г.' => 25],
+            ['с 10 июля 2000 г.' => 28],
+            ['с 21 марта 2000 г.' => 33],
+            ['с 7 марта 2000 г.' => 38],
+            ['с 24 января 2000 г.' => 45],
+            ['с 10 июня 1999 г.' => 55],
+            ['с 24 июля 1998 г.' => 60],
+            ['с 29 июня 1998 г.' => 80],
+            ['с 5 июня 1998 г.' => 60],
+            ['с 27 мая 1998 г.' => 150],
+            ['с 19 мая 1998 г.' => 50],
+            ['с 16 марта 1998 г.' => 30],
+            ['со 2 марта 1998 г.' => 36],
+            ['с 17 февраля 1998 г.' => 39],
+            ['со 2 февраля 1998 г.' => 42],
+            ['с 11 ноября 1997 г.' => 28],
+            ['с 6 октября 1997 г.' => 21],
+            ['с 16 июня 1997 г.' => 24],
+            ['с 28 апреля 1997 г.' => 36],
+            ['с 10 февраля 1997 г.' => 42],
+            ['со 2 декабря 1996 г.' => 48],
+            ['с 21 октября 1996 г.' => 60],
+            ['с 19 августа 1996 г.' => 80],
+            ['с 24 июля 1996 г.' => 110],
+            ['с 10 февраля 1996 г.' => 120],
+            ['с 1 декабря 1995 г.' => 160],
+            ['с 24 октября 1995 г.' => 170],
+            ['с 19 июня 1995 г.' => 180],
+            ['с 16 мая 1995 г.' => 195],
+            ['с 6 января 1995 г.' => 200],
+            ['с 17 ноября 1994 г.' => 180],
+            ['с 12 октября 1994 г.' => 170],
+            ['с 23 августа 1994 г.' => 130],
+            ['с 1 августа 1994 г.' => 150],
+            ['с 30 июня 1994 г.' => 155],
+            ['с 22 июня 1994 г.' => 170],
+            ['со 2 июня 1994 г.' => 185],
+            ['с 17 мая 1994 г.' => 200],
+            ['с 29 апреля 1994 г.' => 205],
+            ['с 15 октября 1993 г.' => 210],
+            ['с 23 сентября 1993 г.' => 180],
+            ['с 15 июля 1993 г.' => 170],
+            ['с 29 июня 1993 г.' => 140],
+            ['с 22 июня 1993 г.' => 120],
+            ['со 2 июня 1993 г.' => 110],
+            ['с 30 марта 1993 г.' => 100],
+            ['с 23 мая 1992 г.' => 80],
+            ['с 10 апреля 1992 г.' => 50],
+            ['с 1 января 1992 г.' => 20],
+        )
+    ];
+
+    my $key = _date_key($date);
+    my $record = first {$key ge $_->[0]} @$bank_rate_changes;
+    croak "no record for $key" if !$record;
+
+    return $record->[1];
+}
+
+sub _get_RU_tax_threshold {
+    my ($date, $currency) = @_;
+    $currency //= 'RUB';
+    return 9  if $currency ne 'RUB';
+
+    my $key = _date_key($date);
+    my $bank_rate = _get_RU_bank_rate($key);
+
+    if ($key ge '2014-12-15' && $key le '2015-12-31') {
+        return $bank_rate + 10;
+    }
+    return $bank_rate + 5;
+}
+
+sub _get_RU_tax_rate {
+    my ($date, $rate, $currency) = @_;
+
+    my $threshold = _get_RU_tax_threshold($date, $currency);
+    return 0 if $rate <= $threshold;
+    return ($rate-$threshold)/$rate * 0.35;
 }

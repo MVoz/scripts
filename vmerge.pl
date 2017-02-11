50d1eba8f1cfe31dda0c4e97fea5c549303c40e2
liosha <xliosha@gmail.com>
2017-02-11 17:06:04 +0300

    merge video files

---
+++ vmerge.pl
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/perl
+
+use 5.018;
+use Path::Tiny;
+
+my @exts = qw/avi mp4 mkv/;
+
+for my $ext (@exts) {
+    my @files = glob "*.$ext";
+    next if !@files;
+
+    my $list = "$ext.list";
+    my $lf = path($list);
+    $lf->spew(map {"file '$_'\n"} @files);
+
+    say "ffmpeg -f concat -safe 0 -i $lf -c copy merged.$ext";
+    `ffmpeg -f concat -safe 0 -i $lf -c copy merged.$ext`;
+}
+
+

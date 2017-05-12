5b1dfdf473b24abe3d4027462ee800e8f8d67f6e
liosha <xliosha@gmail.com>
2017-05-12 21:23:52 +0300

    mask

--- vmerge.pl
+++ vmerge.pl
@@ -3,17 +3,20 @@
 use 5.018;
 use Path::Tiny;
 
-my @exts = qw/avi mp4 mkv/;
+my @exts = qw/avi mp4 mkv mp3/;
+
+my $mask = shift @ARGV || '*';
 
 for my $ext (@exts) {
-    my @files = glob "*.$ext";
+    my @files = glob "$mask.$ext";
     next if !@files;
 
     my $list = "$ext.list";
     my $lf = path($list);
     $lf->spew(map {"file '" . s/'/'\\''/gr ."'\n"} @files);
 
-    my $cmd = "ffmpeg -f concat -safe 0 -i $lf -c copy merged.$ext";
+    my $out = join '_' => grep {$_} map {s/[*?]//gr} ($mask, 'merged');
+    my $cmd = "ffmpeg -f concat -safe 0 -i $lf -c copy $out.$ext";
     `$cmd`;
     say $cmd;
 }
